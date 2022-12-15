18de9fd8155cae3a12fa7321ed05d4907adddec2
ElyseWu <99052684+ElyseWu@users.noreply.github.com>
2022-12-15 12:27:38 -0800

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = 'token';
+// export const BASE_URL = 'https://bobapp-296503.uc.r.appspot.com';
+export const BASE_URL = 'https://possible-cocoa-365616.wm.r.appspot.com';
+// export const BASE_URL = 'https://35.203.134.112:8080';
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+};
